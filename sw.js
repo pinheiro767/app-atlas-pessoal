e3b490847f9cee12304a4f73cff77c970ee83a5d
pinheiro767 <claudiapinheiro767@gmail.com>
2026-05-07 20:47:28 -0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "anatomia-pwa-v3";
+
+const ASSETS_CACHE = [
+    "/",
+    "/index.html",
+    "/style.css",
+    "/app.js",
+    "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(ASSETS_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+                const clone = response.clone();
+
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, clone);
+                });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
+});
